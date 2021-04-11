ee5d4409a93cf94c746da822429e91b73a3b3975
KillerQueen-BitesZaDusto <abusadeed@iut-dhaka.edu>
2021-04-11 21:16:09 +0600

    Search is functional

--- app.js
+++ app.js
@@ -3,11 +3,32 @@ const auth = "563492ad6f91700001000001e74ab2811e314eddad39bc7387fc1fc8"
 const gallery = document.querySelector('.gallery')
 const searchInput = document.querySelector('.search-input')
 const submitButton = document.querySelector('.submit-btn')
+const form = document.querySelector('.search-form')
 
 let searchValue
 
-async function curatedPhotos() {
-	const getData = await fetch('https://api.pexels.com/v1/curated?per_page=15&page=1', {
+searchInput.addEventListener('input', updateInput)
+
+function updateInput(e) {
+	searchValue = e.target.value
+
+}
+
+
+form.addEventListener('submit', (e) => {
+	e.preventDefault()
+	searchPhotos(searchValue)
+})
+
+
+function clear() {
+	gallery.innerHTML = ''
+	searchInput.value = ''
+}
+
+
+async function dataFetch(url) {
+	const getData = await fetch(url, {
 		method: 'GET',
 		headers: {
 			Accept: 'application/json',
@@ -16,7 +37,12 @@ async function curatedPhotos() {
 	})
 
 	const data = await getData.json()
+	return data
 
+}
+
+function showImage(data) {
+	clear()
 	data.photos.forEach(photo => {
 		const galleryImage = document.createElement('div')
 		galleryImage.classList.add('gallery-image')
@@ -24,6 +50,19 @@ async function curatedPhotos() {
 		<p>${photo.photographer}</p>`
 		gallery.appendChild(galleryImage)
 	})
+}
+
+
+async function curatedPhotos() {
+	const data = await dataFetch('https://api.pexels.com/v1/curated?per_page=15&page=1')
+
+	showImage(data)
+}
+
+async function searchPhotos(query) {
+	const data = await dataFetch(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`)
+
+	showImage(data)
 }
 
 curatedPhotos()
