05fea6fa95f0e65a4c1dce1aee6f92f640d32d13
KillerQueen-BitesZaDusto <abusadeed@iut-dhaka.edu>
2021-04-12 20:33:34 +0600

    Added page navifation functionality

--- app.js
+++ app.js
@@ -4,10 +4,42 @@ const gallery = document.querySelector('.gallery')
 const searchInput = document.querySelector('.search-input')
 const submitButton = document.querySelector('.submit-btn')
 const form = document.querySelector('.search-form')
+const next = document.querySelector('.next-btn')
+const prev = document.querySelector('.prev-btn')
+let pageNum = 1
 
 let searchValue
+let currentSearch
 
 searchInput.addEventListener('input', updateInput)
+next.addEventListener('click', updateNext)
+prev.addEventListener('click', updatePrev)
+
+function updateNext(e) {
+	e.preventDefault()
+	pageNum++
+	if (currentSearch) {
+		searchPhotos(currentSearch, pageNum)
+	} else {
+		curatedPhotos(pageNum)
+	}
+
+}
+
+function updatePrev(e) {
+	e.preventDefault()
+	if (pageNum <= 1) {
+		pageNum = 1
+	} else {
+		pageNum--
+	}
+
+	if (currentSearch) {
+		searchPhotos(currentSearch, pageNum)
+	} else {
+		curatedPhotos(pageNum)
+	}
+}
 
 function updateInput(e) {
 	searchValue = e.target.value
@@ -17,6 +49,7 @@ function updateInput(e) {
 
 form.addEventListener('submit', (e) => {
 	e.preventDefault()
+	currentSearch = searchValue
 	searchPhotos(searchValue)
 })
 
@@ -47,22 +80,27 @@ function showImage(data) {
 		const galleryImage = document.createElement('div')
 		galleryImage.classList.add('gallery-image')
 		galleryImage.innerHTML = `<img src=${photo.src.landscape}></img>
-		<p>${photo.photographer}</p>`
+		<div class='photo-info'>
+		<p>${photo.photographer}</p>
+		<button>
+			<a href=${photo.src.original} class='button'>Download</a>
+		</button>
+		</div>`
 		gallery.appendChild(galleryImage)
 	})
 }
 
 
-async function curatedPhotos() {
-	const data = await dataFetch('https://api.pexels.com/v1/curated?per_page=15&page=1')
+async function curatedPhotos(pageNum) {
+	const data = await dataFetch(`https://api.pexels.com/v1/curated?per_page=15&page=${pageNum}`)
 
 	showImage(data)
 }
 
-async function searchPhotos(query) {
-	const data = await dataFetch(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=1`)
+async function searchPhotos(query, pageNum) {
+	const data = await dataFetch(`https://api.pexels.com/v1/search?query=${query}&per_page=15&page=${pageNum}`)
 
 	showImage(data)
 }
 
-curatedPhotos()
+curatedPhotos(pageNum)
